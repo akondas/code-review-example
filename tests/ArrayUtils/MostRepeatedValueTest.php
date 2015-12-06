57378da1727888ca9fb66e3b3222bb5d9e556719
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2015-12-06 16:25:33 +0100

    Update MostRepeatedValueTest.php

--- tests/ArrayUtils/MostRepeatedValueTest.php
+++ tests/ArrayUtils/MostRepeatedValueTest.php
@@ -12,15 +12,6 @@ class MostRepeatedValueTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(1, $mostRepeatedValue);
     }
 
-    public function testIfItReturnMostRepeatedValueInFloatsArray()
-    {
-        $array = [0.33, 0.66, 0.99, 0.33, 0.71, 6.99];
-        $utils = new ArrayUtils();
-        $mostRepeatedValue = $utils->mostRepeatedValue($array);
-
-        $this->assertEquals(0.33, $mostRepeatedValue);
-    }
-
     public function testIfItReturnMostRepeatedValueInStringArray()
     {
         $array = ['Apple', 'Orange', 'Plum', 'Apple', 'Grape', 'Apple'];
